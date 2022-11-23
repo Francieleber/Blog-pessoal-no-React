f4d34330e93890ec6c9348dd2ae294c5d9ac6cb5
francieleber <francielebernardino545@gmail.com>
2022-11-23 12:30:35 -0300

    atualizado

---
+++ src/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css'
+
+function Home (){
+    return(
+        <>
+        <h1 className="titulo">Havaianas</h1>
+        
+
+        <div className="container">
+        <img className="imagem"  src="https://th.bing.com/th/id/OIP.dt4AfV6I0rosRANI61zVegHaDV?pid=ImgDet&rs=1" alt="" />
+        </div>
+        </>
+    )
+}
+export default Home 
